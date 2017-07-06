daf750554f50cc1feed37b19a5b35260adf0391c
Lei Zhang <zhanglei@microduino.cc>
2017-07-06 15:16:02 +0800

    update

--- userDef.h
+++ userDef.h
@@ -1,6 +1,6 @@
-#define DEBUG        1       //串口调试功能  打开/关闭.
-#define PIN_BUZZER   10      //蜂鸣器引脚.
-#define PIN_LED      12      //彩灯引脚.
+#define DEBUG        0       //串口调试功能  打开/关闭.
+#define PIN_BUZZER   2      //蜂鸣器引脚.
+#define PIN_LED      4      //彩灯引脚.
 #define PIN_LINEA    A0      //灰度传感器A引脚.
 #define PIN_LINEB    A2      //灰度传感器B引脚.
 
